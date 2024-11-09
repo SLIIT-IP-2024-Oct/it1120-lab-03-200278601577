06c6df3785295ad92b984445cf17d66132bff51d
200278601577 <it24103825@my.sliit.lk>
2024-11-09 20:30:48 +0530

    Add files via upload

---
+++ Q1B/IT24103825Lab3Q1B.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class IT24103825Lab3Q1B{
+    public static void main(String[] args){
+
+        double pricePerKg,quantity,totalBill,discount,discountedfinalAmount;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter price of 1kg of rice:");
+        pricePerKg = input.nextDouble(); 
+
+        System.out.print("Enter the quantity (kg) you want to buy:");
+        quantity = input.nextDouble();
+        
+        totalBill = pricePerKg * quantity;
+
+        discount = totalBill * 0.10;
+
+        discountedfinalAmount = totalBill - discount;
+
+        System.out.println("The total amount with 10% discount is: " + discountedfinalAmount);
+        
+
+        }
+} 
